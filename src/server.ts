58a18f57460acb722a3da2b88761533184cd13e7
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-04-02 09:46:42 -0300

    refactor: change route to function createEvent

--- src/server.ts
+++ src/server.ts
@@ -4,53 +4,14 @@ import { Schema, z } from 'zod';
 import { prisma } from "./lib/prisma";
 import { TilingPattern } from "jspdf";
 import { generateSlug } from "./utils/generation-slug";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app
-.withTypeProvider<ZodTypeProvider>()
-.post('/events', {
-    schema: {
-        body: z.object({
-            title: z.string().min(4),
-            details: z.string().nullable(),
-            maximunAttendes: z.number().int().positive().nullable()
-        }),
-        response: {
-            201: z.object({
-                eventId: z.string().uuid()
-            })
-        }
-    }
-}, async (request, reply) => {
-    const { title, details, maximunAttendes } = request.body;
-
-    const slug = generateSlug(title);
-
-    const evetnWithSameSlug = await prisma.event.findUnique({
-        where: {
-            slug: slug
-        }
-    })
-
-    if(evetnWithSameSlug){
-        throw new Error("Event already exists")
-    }
-
-    const event = await prisma.event.create({
-        data: {
-            title: title,
-            details: details,
-            maximunAttendes: maximunAttendes,
-            slug
-        }
-    })
-
-    return reply.status(201).send({ eventId: event.id });
-})
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("server is running")
