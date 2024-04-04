19c703f61d37ef3b9886136171622a3e553dc11a
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-04-03 21:47:43 -0300

    feat: add function getEventAttendees

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import fastify from "fastify";
-import { serializerCompiler, ZodTypeProvider, validatorCompiler } from 'fastify-type-provider-zod'
-import { Schema, z } from 'zod';
-import { prisma } from "./lib/prisma";
-import { TilingPattern } from "jspdf";
-import { generateSlug } from "./utils/generation-slug";
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
@@ -20,6 +17,7 @@ app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("server is running")
