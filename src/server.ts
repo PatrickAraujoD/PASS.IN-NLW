eb093f886baed6b580e2b9a97665aea1d29fb36b
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-04-03 23:10:03 -0300

    refactor: change to application receive several platforms

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import fastify from "fastify";
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import fastifyCors from '@fastify/cors'
+import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -9,8 +10,11 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
+app.register(fastifyCors, {
+    origin: '*'
+})
 app.register(fastifySwagger, {
     swagger: {
         consumes: ['application/json'],
@@ -38,6 +42,6 @@ app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
     console.log("server is running")
 })
